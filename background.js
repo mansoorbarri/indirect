c6cd5f3ea4ae2c7cd3d488c4d1636a2cf3286f12
Mansoor Barri <mansoor.eb.ak@gmail.com>
2024-04-09 10:51:30 +0100

    init

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    const url = new URL(details.url);
+    if (url.hostname === "www.instagram.com" || url.hostname === "instagram.com") {
+      if (url.pathname === "/" || url.pathname.startsWith("/explore") || url.pathname.startsWith("/reels")) {
+        const newUrl = "https://www.instagram.com/direct/inbox/";
+        chrome.tabs.update(details.tabId, { url: newUrl });
+      }
+    }
+  }, { url: [{ hostEquals: "www.instagram.com" }, { hostEquals: "instagram.com" }] });
+  
